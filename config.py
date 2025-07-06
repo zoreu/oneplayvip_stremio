5f426827092d1dcf3913782ee666da5d5cb4c821
joelsinhox <joelsinhox@gmail.com>
2025-07-05 23:33:31 -0300

    update

---
+++ config.py
@@ -0,0 +1,7 @@
+config = {
+    'title': 'OnePlay VIP',
+    'version': '1.0.0',
+    'logo': 'https://raw.githubusercontent.com/zoreu/oneplayvip_stremio/refs/heads/main/icon.png',
+    'description': 'OnePlay VIP, tenha sua conta pelo whatsapp +55 31 9594-9574',
+    'xtream_host': 'http://cdn.club8.ca'  # Substitua pelo host fixo do seu servidor Xtream Codes
+}
